7acff62c1484e78ac973cb3724e9e60371b04a45
Alex Brew <abrew840@gmail.com>
2021-10-25 19:52:34 +0100

    databases

--- Databases/Databases.py
+++ Databases/Databases.py
@@ -1,1 +1,20 @@
 import mysql.connector
+
+#Connecting
+
+mydb = mysql.connector.connect(
+    database = "Database"
+)
+
+#Creating cursor
+c = mydb.cursor()
+
+#Create database
+c.execute("CREATE DATABASE Database")
+
+#Check if Database Exists
+
+c.execute("SHOW DATABASES")
+
+for i in c:
+    print(i)

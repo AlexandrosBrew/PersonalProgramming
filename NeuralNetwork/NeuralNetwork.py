d6dc22cdf8bb6b89ca0e406b954d62ce3f0eb568
Alex Brew <abrew840@gmail.com>
2021-11-05 09:16:18 +0000

    nural net

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -14,7 +14,7 @@ class NeuralNetwork:
         return self._sigmoid(x) * (1 - self._sigmoid(x))
         
     def predict(self, input_vector):
-        layer_1= np.dot(input_vector, self.weights) +self.bias
+        layer_1= np.dot(input_vector, self.weights) + self.bias
         layer_2 = self._sigmoid(layer_1)
         prediction = layer_2
         return prediction        
@@ -50,7 +50,7 @@ class NeuralNetwork:
             input_vector = input_vectors[random_data_index]
             target = targets[random_data_index]
 
-            #Compte the gradients and update the weights
+            #Compute the gradients and update the weights
             derror_dbias, derror_dweights = self._compute_gradients(input_vector, target)
 
             self._update_parameters(derror_dbias, derror_dweights)
@@ -83,16 +83,18 @@ input_vectors = np.array(
         [1, 1],
     ]
 )
+def main():
+    targets = np.array([0 , 1, 0, 1, 0, 1, 0, 1])
+    learning_rate = 0.1
 
-targets = np.array([0 , 1, 0, 1, 0, 1, 1, 0])
+    neural_net = NeuralNetwork(learning_rate)
 
-learning_rate = 0.1
+    training_error = neural_net.train(input_vectors, targets, 10000)
 
-neural_net = NeuralNetwork(learning_rate)
+    plt.plot(training_error)
+    plt.xlabel("Iterations")
+    plt.ylabel("Error for all training instances")
+    plt.savefig("cumulative-error.png")
 
-training_error = neural_net.train(input_vectors, targets, 10000)
-
-plt.plot(training_error)
-plt.xlabel("Iterations")
-plt.ylabel("Error for all training instances")
-plt.savefig("cumulative-error.png")
+if __name__ == "__main__":
+    main()
--- Pygame/PygamePractice.py
+++ Pygame/PygamePractice.py
@@ -1,20 +1,45 @@
 import pygame as py
 import os
 
-FPS, WHITE, WIDTH, HEIGHT = 60, (255, 255, 255), 900, 500
-YELLOW_SS_IMG = py.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
-RED_SS_IMG = py.image.load(os.path.join('Assets', 'spaceship_red.png')) 
+FPS, WHITE, BLACK, WIDTH, HEIGHT, VEL = 60, (255, 255, 255), (0, 0, 0), 900, 500, 5
+SS_WIDTH, SS_HEIGHT = 55, 40
+
+YELLOW_SS_IMG = py.image.load(os.path.join('/Users/alexandrosbrew/Documents/PersonalProgramming/Pygame/Assets/spaceship_yellow.png'))
+YELLOW_SS_IMG = py.transform.scale(YELLOW_SS_IMG, (SS_WIDTH, SS_HEIGHT))
+YELLOW_SS_IMG = py.transform.rotate(YELLOW_SS_IMG, -90)
+RED_SS_IMG = py.image.load(os.path.join('/Users/alexandrosbrew/Documents/PersonalProgramming/Pygame/Assets/spaceship_red.png')) 
+RED_SS_IMG = py.transform.scale(RED_SS_IMG, (SS_WIDTH, SS_HEIGHT))
+RED_SS_IMG = py.transform.rotate(RED_SS_IMG, 90)
+
+BORDER = py.Rect((WIDTH/2)-5, 0 , 10, HEIGHT)
 WIN = py.display.set_mode((WIDTH, HEIGHT))
 py.display.set_caption("Spaceships")
 
 
-def draw_window():
+def draw_window(red, yellow):
     WIN.fill(WHITE)
-    WIN.blit(YELLOW_SS_IMG, (675, 375))
-    WIN.blit(RED_SS_IMG, (225, 125))
+    py.draw.rect(WIN, BLACK, BORDER)
+    WIN.blit(YELLOW_SS_IMG, (yellow.x, yellow.y))
+    WIN.blit(RED_SS_IMG, (red.x, red.y))
     py.display.update()
 
+def r_move(keys_pressed, red):
+    if keys_pressed[py.K_a]: red.x -= VEL #Left
+    if keys_pressed[py.K_d]: red.x += VEL #Right
+    if keys_pressed[py.K_w]: red.y -= VEL #Up
+    if keys_pressed[py.K_s]: red.y += VEL #Down
+
+def y_move(keys_pressed, yellow):
+    if keys_pressed[py.K_LEFT] and yellow.x + VEL + yellow.width < BORDER.x: yellow.x -= VEL #Left
+    if keys_pressed[py.K_RIGHT] and yellow.x - VEL > 0: yellow.x += VEL #Right
+    if keys_pressed[py.K_UP] and yellow.y - VEL > 0: yellow.y -= VEL #Up
+    if keys_pressed[py.K_DOWN] and yellow.y + VEL + yellow.height < HEIGHT: yellow.y += VEL #Down
+    
+
 def main():
+    red = py.Rect(100, 300, SS_WIDTH, SS_HEIGHT)
+    yellow = py.Rect(700, 300, SS_WIDTH, SS_HEIGHT)
+
     clock = py.time.Clock()
     run = True
     while run:
@@ -23,7 +48,10 @@ def main():
             if event.type == py.QUIT:
                 run = False
 
-        draw_window()
+        keys_pressed = py.key.get_pressed()
+        r_move(keys_pressed, red)
+        y_move(keys_pressed, yellow)
+        draw_window(red, yellow)
 
     py.quit()
 

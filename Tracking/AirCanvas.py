609999b108ffffb9e2c5a090ffa0acb7c36b33a5
Alex Brew <abrew840@gmail.com>
2021-11-08 15:35:12 +0000

    AirCanvasScreenShotCapability

--- Tracking/AirCanvas.py
+++ Tracking/AirCanvas.py
@@ -2,6 +2,7 @@
 import cv2
 import numpy as np
 
+i = 0
 colors = [(255, 0, 0), (255, 0, 255), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
 color = colors[0]
 min_area = 1000
@@ -62,6 +63,11 @@ while True:
                     
                     elif cX > 540 and cX < 620:
                         color = colors[4]
+                    elif cX > 640 and cX < 780:
+                        return_val, image = cap.read()
+                        cv2.imwrite('screenshot'+str(i+1)+'.png', canvas)
+                        i+=1
+
             if previous_center_point != 0:
                 cv2.line(canvas, previous_center_point, (cX, cY), color, 2)
             previous_center_point = (cX, cY)
@@ -81,12 +87,14 @@ while True:
     cv2.rectangle(frame, (340,1), (420,65), colors[2], -1)
     cv2.rectangle(frame, (440,1), (520,65), colors[3], -1)
     cv2.rectangle(frame, (540,1), (620,65), colors[4], -1)
+    cv2.rectangle(frame, (640, 1), (780, 65), (122, 122, 122), -1)
     cv2.putText(frame, "CLEAR ALL", (30, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.putText(frame, "BLUE", (155, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.putText(frame, "VIOLET", (255, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.putText(frame, "GREEN", (355, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.putText(frame, "RED", (465, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.putText(frame, "YELLOW", (555, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "SCREENSHOT", (665, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.imshow("Frame", frame)
     cv2.imshow('Canvas', canvas)
 

95e2068d65a046182158bf7cbac8057bf1e023c8
Alex Brew <abrew840@gmail.com>
2021-11-08 18:50:50 +0000

    NeuralNet

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -8,17 +8,17 @@ class NeuralNetwork:
         self.learning_rate = learning_rate
 
     def _sigmoid(self, x):
-        return 1 / (1 +np.exp(x))
-    
+        return 1 / (1 + np.exp(-x))
+
     def _sigmoid_deriv(self, x):
         return self._sigmoid(x) * (1 - self._sigmoid(x))
-        
+
     def predict(self, input_vector):
-        layer_1= np.dot(input_vector, self.weights) + self.bias
+        layer_1 = np.dot(input_vector, self.weights) + self.bias
         layer_2 = self._sigmoid(layer_1)
         prediction = layer_2
-        return prediction        
-    
+        return prediction
+
     def _compute_gradients(self, input_vector, target):
         layer_1 = np.dot(input_vector, self.weights) + self.bias
         layer_2 = self._sigmoid(layer_1)
@@ -30,49 +30,54 @@ class NeuralNetwork:
         dlayer1_dweights = (0 * self.weights) + (1 * input_vector)
 
         derror_dbias = (
-            derror_dprediction* dprediction_dlayer1 * dlayer1_dbias
+            derror_dprediction * dprediction_dlayer1 * dlayer1_dbias
         )
-        derror_dweights= (
+        derror_dweights = (
             derror_dprediction * dprediction_dlayer1 * dlayer1_dweights
         )
+
         return derror_dbias, derror_dweights
 
     def _update_parameters(self, derror_dbias, derror_dweights):
         self.bias = self.bias - (derror_dbias * self.learning_rate)
-        self.weights = self.weights - (derror_dweights * self.learning_rate)
-    
+        self.weights = self.weights - (
+            derror_dweights * self.learning_rate
+        )
+
     def train(self, input_vectors, targets, iterations):
-        cumulactive_errors = []
-        for current_itteration in range(iterations):
-            #Pick a data instance at random
-            random_data_index = np.random.randint(len(input_vectors))
+            cumulative_errors = []
+            for current_iteration in range(iterations):
+                # Pick a data instance at random
+                random_data_index = np.random.randint(len(input_vectors))
 
-            input_vector = input_vectors[random_data_index]
-            target = targets[random_data_index]
+                input_vector = input_vectors[random_data_index]
+                target = targets[random_data_index]
 
-            #Compute the gradients and update the weights
-            derror_dbias, derror_dweights = self._compute_gradients(input_vector, target)
+                # Compute the gradients and update the weights
+                derror_dbias, derror_dweights = self._compute_gradients(
+                    input_vector, target
+                )
 
-            self._update_parameters(derror_dbias, derror_dweights)
+                self._update_parameters(derror_dbias, derror_dweights)
 
-            #measure the cumulative error for all the instances
-            if current_itteration % 100 == 0:
-                cumulactive_error = 0
-                #Loop through all the instances to measure the error
-                for data_instance_index in range(len(input_vectors)):
-                    data_point = input_vectors[data_instance_index]
-                    target = targets[data_instance_index]
+                # Measure the cumulative error for all the instances
+                if current_iteration % 100 == 0:
+                    cumulative_error = 0
+                    # Loop through all the instances to measure the error
+                    for data_instance_index in range(len(input_vectors)):
+                        data_point = input_vectors[data_instance_index]
+                        target = targets[data_instance_index]
 
-                    prediction = self.predict(data_point)
-                    error = np.square(prediction - target)
+                        prediction = self.predict(data_point)
+                        error = np.square(prediction - target)
 
-                    cumulactive_error = cumulactive_error + error
-                cumulactive_errors.append(cumulactive_error)
-            
-            return cumulactive_errors
+                        cumulative_error = cumulative_error + error
+                    cumulative_errors.append(cumulative_error)
+
+            return cumulative_errors
 
 input_vectors = np.array(
-    [
+     [
         [3, 1.5],
         [2, 1],
         [4, 1.5],
@@ -82,19 +87,17 @@ input_vectors = np.array(
         [5.5, 1],
         [1, 1],
     ]
-)
+)        
 def main():
-    targets = np.array([0 , 1, 0, 1, 0, 1, 0, 1])
+    targets = np.array([0, 1, 0, 1, 0, 1, 1, 0])
     learning_rate = 0.1
+    neuralnet = NeuralNetwork(learning_rate)
+    trainingerr = neuralnet.train(input_vectors, targets, 100000)
 
-    neural_net = NeuralNetwork(learning_rate)
-
-    training_error = neural_net.train(input_vectors, targets, 10000)
-
-    plt.plot(training_error)
+    plt.plot(trainingerr)
     plt.xlabel("Iterations")
     plt.ylabel("Error for all training instances")
-    plt.savefig("cumulative-error.png")
+    plt.savefig("Cumulative_error.png")
 
 if __name__ == "__main__":
     main()

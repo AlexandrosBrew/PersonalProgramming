78db2ab5e53c79e9511d09b79388e8b6685f2199
Alex Brew <abrew840@gmail.com>
2021-11-18 15:10:49 +0000

    Net

--- NeuralNetwork/NeuralNetworkModule.py
+++ NeuralNetwork/NeuralNetworkModule.py
@@ -78,11 +78,13 @@ class NeuralNetwork:
             return cumulative_errors
 
 def main(input_vectors):
+    input_vectors = [] #!([1.5, 4], [3, 5], [8,0.5], [12, 3], [4,6], [.30,1], [9.8, 2], [10, 3.4])
 
-    rand1 = random.randrange(0, 10)
-    rand2 = random.randrange(0, 10)
-
-    input_vectors = np.array([rand1, rand2])
+    for y in range(0, 10):
+        rand1 = random.randrange(0, 10)
+        rand2 = random.randrange(0, 10)
+        input_vectors.append(np.array([rand1, rand2]))
+    targets = [0 , 1, 0, 0, 1, 0, 1, 0]
     learning_rate = 0.1
     neuralnet = NeuralNetwork(learning_rate)
     trainingerr = neuralnet.train(input_vectors, targets, 100000)
